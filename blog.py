6f8d364ef565489e3863e30907b100ec31546930
fromLittleAcorns <confusedjohn46@gmail.com>
2026-02-17 11:37:14 +0000

    Converted to nbdev_v3.  Updated code in preparation to moving to prod

--- blog.py
+++ blog.py
@@ -1,10 +1,11 @@
 from fasthtml.common import FastHTML, serve
 from my_blog import core_v5
 from my_blog.core_v5 import *
+import my_blog.config as config
 
 # Initialize and run the app
 app, state = create_app()
 core_v5.state = state
 register_routes(app)  # Register all @route decorated handlers
 # srv = JupyUvi(app)  # For notebook testing
-serve(port=8000)
+serve(port=config.PORT)
