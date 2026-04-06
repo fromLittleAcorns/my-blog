75ddf0c4f145bb27e1e16b8e9bbf9227e87a2bc9
Solveit <nobody@fast.ai>
2026-04-06 10:33:42 +0000

    bug fix

--- blog.py
+++ blog.py
@@ -1,5 +1,7 @@
 from fasthtml.common import FastHTML, serve
 from my_blog import core_v5
+from my_blog.admin_v1 import (register_admin_routes, post_edit, save_post, replace_iframe, replace_strava, post, 
+    load_post, edit_layout, delete_post, download_post)
 from my_blog.core_v5 import *
 import my_blog.config as config
 
